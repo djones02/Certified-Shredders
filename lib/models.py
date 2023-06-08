ba3099f835b774cc2846853ccc6a4564b5433fcc
Eli Dawson <edawson@alumni.berklee.edu>
2023-06-08 14:26:16 -0400

    poser vs shredder functionality

--- lib/models.py
+++ lib/models.py
@@ -10,7 +10,7 @@ class User(Base):
 
     id = Column(Integer(), primary_key=True)
     name = Column(String(), index=True)
-    certified = Column(String(), default="False")
+    certified = Column(String(), default="Poser")
     reviews = Column(Integer(), default=0)
 
     def __repr__(self):
